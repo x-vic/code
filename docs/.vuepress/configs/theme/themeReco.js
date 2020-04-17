ca717935c4f8e5942c19e1bfb3957103a4003c84
jimixy <2310385247@qq.com>
2020-04-17 10:19:00 +0800

    add: owner-vue-next

--- docs/.vuepress/configs/theme/themeReco.js
+++ docs/.vuepress/configs/theme/themeReco.js
@@ -12,43 +12,19 @@ module.exports = {
   lastUpdated: '上次更新', // string | boolean
   // 作者
   author: 'Vic',
-  // authorAvatar: '/head.png',
-  // 备案号
-  // record: '京ICP备17067634号-1',
-  // 项目开始时间
   startYear: '2020',
-  // valine 设置
-  // valineConfig: {
-  //   appId: 'jvc9s4BkJYQNOcpsbVTPMePe-gzGzoHsz',
-  //   appKey: 'Js91M9DfM9vPwVaUj7xdkbxh',
-  //   placeholder: '填写邮箱可以收到回复提醒哦！',
-  //   notify: true,
-  //   recordIP: true
-  // },
   comment: {
     type: 'valine',
     appId: 'RJWm2XhTmAA3kXA5k8q3kGKD-gzGzoHsz',
     appKey: 'MCNl516YKivtqhEAmr8YaMtY'
   },
   // 假定是 GitHub. 同时也可以是一个完整的 GitLab URL
-  repo: 'https://www.github.com/yesixuan/vue-code',
-  // // 假如文档不是放在仓库的根目录下：
+  // repo: 'https://www.github.com/yesixuan/vue-code',
   docsDir: 'docs',
-  // // 假如文档放在一个特定的分支下：
   docsBranch: 'master',
-  // // 默认是 false, 设置为 true 来启用
   // editLinks: true
   markdown: {
     // 启用流程图功能
     flowchart: true,
   },
 }
-
-// github issue 评论
-// vssueConfig: {
-//   platform: 'github',
-//   owner: 'yesixuan',
-//   repo: 'vue-code',
-//   clientId: '43a8e5d8ab5db4be4c93',
-//   clientSecret: 'c1bb3d31de3b99540a2705b081c073dfc4191979'
-// },
